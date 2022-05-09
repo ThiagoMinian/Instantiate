e6f817950fd1284c4c1f5f9c6d783cf5c449618b
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-05-09 12:10:08 -0300

    ClonarCubos

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public int cantclones;
+    public GameObject objectToClone;
+
+    public void CloneObject()
+    {
+        int contador = 0;
+        while (contador < cantclones)
+        {
+            Instantiate(objectToClone);
+            contador++; 
+
+        }
+
+    }
+}
